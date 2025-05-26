9326ef433cae85fe402d14f37f311d715262d1de
Gabriela <leticia.lima@nees.ufal.br>
2025-05-26 12:52:47 -0300

    feat button component

---
+++ components/Button/index.tsx
@@ -0,0 +1,81 @@
+import { Colors } from "@/constants/Colors";
+import { ActivityIndicator, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+type ButtonProps = {
+  label: string;
+  onPress: () => void;
+  disabled?: boolean;
+  loading?: boolean;
+  variant?: 'primary' | 'secondary' | 'tertiary';
+  size?: 'small' | 'medium' | 'large';
+}
+const Button = ({
+  label,
+  onPress,
+  disabled = false,
+  loading = false,
+  variant = 'primary',
+  size = 'medium',
+  ...props
+} : ButtonProps) => {
+  return (
+ <TouchableOpacity
+      onPress={onPress}
+      disabled={disabled}
+      style={[
+        styles.button,
+        styles[variant],
+        disabled  && styles.disabled,
+      ]}
+    >
+      {
+        loading ? (
+          <ActivityIndicator color={Colors.light.text} />
+        ) : (
+          <Text style={[
+            styles[size],
+            {
+              color: disabled  ? Colors.light.textTertiary : Colors.light.text,
+              fontFamily: 'Montserrat-SemiBold',
+            }
+          ]}>
+            {label}
+          </Text>
+        )
+      }
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    borderRadius: 50,
+    alignItems: 'center',
+  },
+  primary: {
+    backgroundColor: Colors.light.primaryButtonBg,
+  },
+  secondary: {
+    backgroundColor: Colors.light.bgGray,
+    borderColor: Colors.light.border,
+    borderWidth: 2,
+  },
+  tertiary: {
+    backgroundColor: 'transparent',
+  },
+  small: {
+    fontSize: 12,
+  },
+  medium: {
+    fontSize: 14,
+  },
+  large: {
+    fontSize: 18,
+  },
+  disabled: {
+    backgroundColor: '#E0E0E0',
+  },
+})
+
+export default Button;
