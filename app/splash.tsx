c3faf4ac4f10854c0fe377f2fd834230e27f74e8
Gabriela <leticia.lima@nees.ufal.br>
2025-05-26 12:53:26 -0300

    feat splash screen

---
+++ app/splash.tsx
@@ -0,0 +1,37 @@
+// SplashScreen.js
+import { Colors } from "@/constants/Colors";
+import { ActivityIndicator, Image, StyleSheet, View } from "react-native";
+
+const Logo = require("@/assets/images/icon.png");
+
+interface Props {
+  absolute?: boolean;
+}
+export function SplashScreen({ absolute }: Props) {
+  return (
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.image} />
+      <ActivityIndicator style={styles.spinner} size="large" color="#FDBD1A" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.light.background,
+  },
+  image: {
+    width: 250,
+    height: 250,
+    borderRadius: 20,
+  },
+  spinner: {
+    position: "absolute",
+    bottom: 100,
+  },
+});
+
+export default SplashScreen;
