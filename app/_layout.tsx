98e451889358e6a6ce1b9efcb2f26dec021e5252
Gabriela <leticia.lima@nees.ufal.br>
2025-05-26 12:53:50 -0300

    feat update no layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useEffect } from 'react';
+import { SplashScreen } from './splash';
 
 export default function RootLayout() {
+  const router = useRouter();
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     'Montserrat-Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
@@ -16,16 +19,45 @@ export default function RootLayout() {
     'Montserrat-Light': require('../assets/fonts/Montserrat-Light.ttf'),
   });
 
+  const isLogged = false;
+
+
+
+  useEffect(() => {
+    if (loaded && isLogged) {
+      router.replace('/(tabs)');
+    }
+  }, [loaded, isLogged]);
+
+
   if (!loaded) {
     // Async font loading only occurs in development.
-    return null;
+    return <SplashScreen />;
   }
+
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="auth"
+          options={{
+            headerShown: false,
+          }}
+          />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
+        <Stack.Screen
+          name="splash"
+          options={{
+            headerShown: false,
+          }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
