4b1559e09411ace306e7d8e5a96dc0e9942ba421
Gabriela <leticia.lima@nees.ufal.br>
2025-06-24 11:04:42 -0300

    feat:  componentes de input de texto e senha com estilos

---
+++ components/Inputs/InputPassword/index.tsx
@@ -0,0 +1,36 @@
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { Colors } from "@/constants/Colors";
+import { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "../styles";
+import { InputProps } from "../types";
+
+export const InputPassword = ({label, id} : InputProps) => {
+  const [data, setData] = useState("");
+  const [inputFocused, setInputFocused] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  }
+  return (
+    <View style={styles.viewInput}>
+      <Text style={styles.labelInput}>{label}</Text>
+      <TextInput
+        style={[styles.input, inputFocused && styles.inputFocused]}
+        placeholder="******"
+        id={id}
+        value={data}
+        onChangeText={setData}
+        onFocus={() => setInputFocused(true)}
+        onBlur={() => setInputFocused(false)}
+        secureTextEntry={!showPassword}
+        autoComplete="password"
+      />
+
+      <TouchableOpacity style={{ position: 'absolute', right: 12, top: 35 }} onPress={handleShowPassword}>
+        <IconSymbol name={showPassword ? 'eye.slash' : 'eye'} size={24} color={Colors.light.icon} />
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ components/Inputs/styles.ts
@@ -0,0 +1,26 @@
+import { Colors } from '@/constants/Colors';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  viewInput: {
+    gap: 8,
+  },
+  labelInput: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 12,
+    marginHorizontal: 8
+  },
+  input: {
+    height: 48,
+    borderColor: Colors.light.border,
+    borderWidth: 1,
+    borderRadius: 16,
+    paddingHorizontal: 12,
+    marginBottom: 20,
+    fontSize: 16
+  },
+  inputFocused: {
+    borderColor: Colors.light.positiveBg,
+    borderWidth: 2
+  },
+});
