8f657aa7dea99c4afeed58305e7e7b072132f4ce
Gabriela <leticia.lima@nees.ufal.br>
2025-06-25 14:19:50 -0300

    feat: atualizar componente Input para suportar formatação de valor e novos tipos

--- src/components/Inputs/types.ts
+++ src/components/Inputs/types.ts
@@ -1,10 +1,14 @@
+import { StyleProp, TextStyle } from "react-native";
+
 export type InputProps = {
   id: string;
-  label: string;
+  type?: "text" | "email" | "number" | "phone" | "money"
+  label?: string;
   placeholder?: string;
   value?: string;
   onChange?: (value: string) => void;
   onFocus?: () => void;
   onBlur?: () => void;
   keyboardType?: "default" | "email-address" | "numeric" | "phone-pad";
+  style?: StyleProp<TextStyle>;
 }
