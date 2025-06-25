abd4ddf8f71dfff0545184a822732b1b8c572248
Gabriela <leticia.lima@nees.ufal.br>
2025-06-25 14:20:09 -0300

    feat: adicionar tela de nova transação com seleção de tipo e modal

---
+++ src/app/new-transaction/index.tsx
@@ -0,0 +1,37 @@
+import { NewTransactionHeader, TransactionTypeModal } from "@/modules/new-transaction/components";
+import { styles } from "@/modules/new-transaction/components/styles";
+import { router } from "expo-router";
+import { useEffect, useState } from "react";
+import { Keyboard, TouchableWithoutFeedback, View } from "react-native";
+
+
+export default function NewTransactionScreen() {
+  const [selectedType, setSelectedType] = useState<"income" | "expense">(
+    "income"
+  );
+  const [openSelectType, setOpenSelectType] = useState(false);
+  useEffect(() => {
+    console.log("Selected type changed:", selectedType);
+  }, [selectedType]);
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <NewTransactionHeader
+          selectedType={selectedType}
+          onClose={() => router.back()}
+          onOpenModal={() => setOpenSelectType(true)}
+          onSave={() => router.back()}
+        />
+
+
+
+        <TransactionTypeModal
+          visible={openSelectType}
+          onClose={() => setOpenSelectType(false)}
+          onSelect={setSelectedType}
+        />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
