ab8d9632e7c1d2143f0ccbd6a5228cbfba3994a3
Gabriela <leticia.lima@nees.ufal.br>
2025-06-27 11:35:31 -0300

    feat: adicionar componente CustomSelect com modal para seleção de opções

---
+++ src/components/CustomSelect/index.tsx
@@ -0,0 +1,47 @@
+import { Colors } from '@/constants/Colors';
+import React, { useState } from 'react';
+import { Modal, Text, TouchableOpacity, View } from 'react-native';
+import { TextStyled } from '../TextStyled';
+import { styles } from './styles';
+
+const data = ['Opção 1', 'Opção 2', 'Opção 3'];
+
+export const CustomSelect = () => {
+  const [selected, setSelected] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.selectBox}>
+        <TextStyled text={selected || 'Selecione uma opção'}
+          color={selected ? Colors.light.text : Colors.light.textTertiary}
+        />
+      </TouchableOpacity>
+
+      <Modal transparent visible={modalVisible} animationType="fade">
+        <TouchableOpacity
+          style={styles.modalBackground}
+          activeOpacity={1}
+          onPressOut={() => setModalVisible(false)}
+        >
+          <View style={styles.modalContent}>
+            {data.map((item) => (
+              <TouchableOpacity
+                key={item}
+                onPress={() => {
+                  setSelected(item);
+                  setModalVisible(false);
+                }}
+                style={styles.option}
+              >
+                <Text>{item}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </TouchableOpacity>
+      </Modal>
+    </View>
+  );
+};
+
