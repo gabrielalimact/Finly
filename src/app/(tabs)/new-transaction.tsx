271d614d01304247d93fe4754be719858cd37950
gabrielalimact <lgcl@ic.ufal.br>
2025-10-15 08:24:56 -0300

    feat: implementar tela de nova transação com campos de entrada e seleção

--- src/app/(tabs)/new-transaction.tsx
+++ src/app/(tabs)/new-transaction.tsx
@@ -2,11 +2,11 @@ import Button from "@/components/Button";
 import { CustomSelect } from "@/components/CustomSelect";
 import { Input } from "@/components/Inputs/Input";
 import { TextStyled } from "@/components/TextStyled";
+import { Colors } from "@/constants/Colors";
 import { NewTransactionHeader, TransactionTypeModal } from "@/modules/new-transaction/components";
-import { styles } from "@/modules/new-transaction/components/styles";
 import { router } from "expo-router";
 import { useState } from "react";
-import { Keyboard, Platform, TouchableWithoutFeedback, View } from "react-native";
+import { Keyboard, Platform, SafeAreaView, ScrollView, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 export default function NewTransactionScreen() {
   const [selectedType, setSelectedType] = useState<"income" | "expense">(
@@ -26,7 +26,8 @@ export default function NewTransactionScreen() {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
+      <SafeAreaView>
+      <ScrollView style={styles.container}>
         <NewTransactionHeader
           selectedType={selectedType}
           onClose={() => router.back()}
@@ -40,7 +41,9 @@ export default function NewTransactionScreen() {
           <Input id="nome-transacao" placeholder="Digite o nome da sua transação" />
 
           <TextStyled text="Conta" fontWeight="medium" padding={8}/>
-          <CustomSelect /> 
+          <CustomSelect 
+            data={["Nubank", "Banco Inter", "C6 Bank", "Banco do Brasil"]}
+          /> 
 
           <TextStyled text="Data da Transação" fontWeight="medium" padding={8}/>
           <Button label={date ? date.toLocaleDateString() : "Selecionar Data"} onPress={() => setDatePickerVisibility(true)} variant="secondary" />
@@ -68,7 +71,42 @@ export default function NewTransactionScreen() {
           }}
           onCancel={() => setDatePickerVisibility(false)}
         />
-      </View>
+      <TouchableOpacity onPress={() => router.back()} style={styles.button}>
+          <TextStyled
+            fontWeight="bold"
+            text={"Salvar"}
+          />
+        </TouchableOpacity>
+      </ScrollView>
+
+      </SafeAreaView>
+
     </TouchableWithoutFeedback>
   );
 }
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.light.bgPrimary,
+  },
+  form: {
+    padding: 20,
+  },
+  button: {
+    marginHorizontal: 20,
+    backgroundColor: Colors.light.primaryButtonBg,
+    height: 40,
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: Colors.light.black
+  },
+  flexRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+});
+
